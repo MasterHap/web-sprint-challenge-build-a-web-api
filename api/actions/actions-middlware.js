ebe9efdea43a8d8f1fb77da303c1dcb5e3bc12d1
MasterHap <76802578+MasterHap@users.noreply.github.com>
2022-01-14 04:55:49 -0500

    Setup Router server and middleware

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,18 @@
 // add middlewares here related to actions
+const Acts = require('./actions-model')
+
+async function checkActId(req, res, next) {
+    try {
+        const possibleAct = await Acts.findById(req.params.id)
+        if (possibleAct) {
+            req.act = possibleAct
+            next()
+        } else {
+            next({ status: 404, message: `No Act ${req.params.id}` })
+        }
+    } catch (err) {
+        next(err)
+    }
+}
+
+module.exports = { checkActId }
--- api/server.js
+++ api/server.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const server = express();
 const actionsRouter = require('./actions/actions-router');
+const projectsRouter = require('./projects/projects-router')
 
 server.use(express.json());
 server.use('api/actions', actionsRouter);
+server.use('api/projects', projectsRouter);
 
 server.get('/', (req, res) => {
     res.send(`
     <h2>Api!!!<h2>
     `)
 })
+
+server.use('*', (req, res) => {
+    res.status(404).json({ message: `${req.method} ${req.baseUrl} not found!` });
+  });
+
+server.use((err, req, res, next) => { // eslint-disable-line
+    console.log('terrible')
+    res.status(err.status || 500).json({
+    message: `Oh Boy: ${err.message}`,
+    })
+  })
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
