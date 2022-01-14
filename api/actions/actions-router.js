bdeb030ab78395908041d518d2c6707310ae1cf7
MasterHap <76802578+MasterHap@users.noreply.github.com>
2022-01-14 05:13:44 -0500

    RouterWIP

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -6,13 +6,17 @@ const { checkActId } = require('./actions-middlware');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    Acts.find(req.query)
+    Acts.get(req.query)
     .then(acts => {
         res.status(200).json(acts);
     })
     .catch(err => {
         next(err)
     })
+})
+
+router.get('/:id', checkActId, (req, res) => {
+    res.json(req.act)
 })
 
 module.exports = router;
