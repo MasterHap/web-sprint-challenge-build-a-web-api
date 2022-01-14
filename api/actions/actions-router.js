2de84910c1d6c81825baffc2b5d7656235a9a1c7
MasterHap <76802578+MasterHap@users.noreply.github.com>
2022-01-14 04:56:10 -0500

    RouterWIP

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,7 +1,18 @@
 // Write your "actions" router here!
 const express = require('express');
-const Act = require('./actions-model');
+const Acts = require('./actions-model');
+const { checkActId } = require('./actions-middlware');
 
 const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    Acts.find(req.query)
+    .then(acts => {
+        res.status(200).json(acts);
+    })
+    .catch(err => {
+        next(err)
+    })
+})
 
 module.exports = router;
